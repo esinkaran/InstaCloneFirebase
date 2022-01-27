412a81100ea49a7bc660a7c6c4180811e2dbdcb9
Esin KARAN <esinkarann@gmail.com>
2022-01-28 02:06:37 +0300

    interface and storage settings

--- InstaCloneFirebase/UploadViewController.swift
+++ InstaCloneFirebase/UploadViewController.swift
@@ -6,24 +6,80 @@
 //
 
 import UIKit
+import Firebase
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+
+        
+        let gestureRegocnizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(gestureRegocnizer)
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func selectImage(){
+        print("click")
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
     }
-    */
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    @IBAction func uploadClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+            
+            let uuid = UUID().uuidString
+            let imageReference = mediaFolder.child("\(uuid).jpeg")
+            imageReference.putData(data, metadata: nil) { metaData, error in
+                
+                if error != nil {
+                    
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error!")
+                }
+                else{
+                    imageReference.downloadURL { url, error in
+                        if error == nil{
+                            
+                            let imageUrl = url?.absoluteString
+                            
+                            print(imageUrl)
+                        }
+                    }
+                }
+            }
+        }
+        
+        
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String){
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+        
+        let button = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(button)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 }
--- InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { authData, error in
+                
+                if error != nil {
+                    
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
+                    
+                }
+                else{
+                    
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != ""{
+            
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { authData, error in
+            
+                if error != nil {
+                    
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
+                }
+                else{
+                    
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }
+        else{
+            
+            makeAlert(titleInput: "Error", messageInput: "Username or password wrong")
+        }
+    }
+    
+    func makeAlert(titleInput: String , messageInput: String){
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let button = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(button)
+        self.present(alert, animated: true, completion: nil)
+        
+        
+    }
 }
 
